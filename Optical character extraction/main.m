b2ea88c46664074e0875057a5e5ff59741ca6716
Joji Jermias <75560207+JojiJermias@users.noreply.github.com>
2023-02-21 14:10:42 +0100

    Add Folder

---
+++ Optical character extraction/main.m
@@ -0,0 +1,138 @@
+function varargout = main(varargin)
+% MAIN MATLAB code for main.fig
+% Begin initialization code
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @main_OpeningFcn, ...
+                   'gui_OutputFcn',  @main_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code
+
+
+% --- Executes just before main is made visible.
+function main_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to main (see VARARGIN)
+
+% Choose default command line output for main
+
+a=imread('ban.jpg');
+axes(handles.axes1);
+imshow(a,[],'XData',[0 .5],'YData',[0 .1]);
+% Choose default command line output for main
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+% UIWAIT makes main wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = main_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+global im;
+[filepath,pathname]=uigetfile({'*.*'},'Select File');
+im=strcat(pathname,filepath);
+im=imread(im);
+
+axes(handles.axes2);
+imshow(im);
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+global im;
+% Read im
+
+
+imshow(im);
+title('INPUT IMAGE');
+imagen=im;
+% Convert to gray scale
+if size(imagen,3)==3 %RGB image
+    imagen=rgb2gray(imagen);
+end
+% Convert to BW
+threshold = graythresh(imagen);
+imagen =~im2bw(imagen,threshold);
+% Remove all object containing fewer than 30 pixels
+imagen = bwareaopen(imagen,30);
+%Storage matrix word from image
+word=[ ];
+re=imagen;
+%Opens text.txt as file for write
+fid = fopen('text.txt', 'wt');
+% Load templates
+load templates
+global templates
+% Compute the number of letters in template file
+num_letras=size(templates,2);
+while 1
+    %Fcn 'lines' separate lines in text
+    [fl re]=lines(re);
+    imgn=fl;
+    %Uncomment line below to see lines one by one
+     %imshow(fl);pause(0.5)    
+    %-----------------------------------------------------------------     
+    % Label and count connected components
+    [L Ne] = bwlabel(imgn);    
+    for n=1:Ne
+        [r,c] = find(L==n);
+        % Extract letter
+        n1=imgn(min(r):max(r),min(c):max(c));  
+        % Resize letter (same size of template)
+        img_r=imresize(n1,[42 24]);
+        %Uncomment line below to see letters one by one
+        %imshow(img_r);pause(0.5)
+        %-------------------------------------------------------------------
+        % Call fcn to convert image to text
+        letter=read_letter(img_r,num_letras);
+        % Letter concatenation
+        word=[word letter];
+    end
+    %fprintf(fid,'%s\n',lower(word));%Write 'word' in text file (lower)
+    fprintf(fid,'%s\n',word);%Write 'word' in text file (upper)
+    % Clear 'word' variable
+    word=[ ];
+    %*When the sentences finish, breaks the loop
+    if isempty(re)  %See variable 're' in Fcn 'lines'
+        break
+    end    
+end
+fclose(fid);
+%Open 'text.txt' file
+winopen('text.txt')
+
+clear all
